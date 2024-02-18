a6906d112785457c80d53a6ea075a1fde1f71c1b
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-02-18 19:35:42 +0530

    implemented the search functionality in the Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,37 +26,33 @@ const Home = () => {
     fetchProducts();
   }, []);
 
-  // const handleSearch = (event) => {
-  //   const term = event.target.value.toLowerCase();
-  //   setSearchTerm(term);
-  //   const filtered = products.filter(product => {
-  //     console.log("Product Name:", product.name);
-  //     console.log("Search Term:", term);
-  //     return product.name && product.name.toLowerCase().includes(term);
-  //   });
-  //   console.log("Filtered Products:", filtered);
-  //   setFilteredProducts(filtered);
-  // };
-  
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = products.filter(product => {
+      return product.title.toLowerCase().includes(term);
+    });
+    setFilteredProducts(filtered);
+  };
 
   return (
-    <div className="" >
+    <div className="">
       <Navbar />
-      {/* <input
+      <input
         type="text"
         placeholder="Search products..."
         value={searchTerm}
         onChange={handleSearch}
         className="border border-gray-300 rounded-md px-3 py-2 mb-4"
-      /> */}
-      <h1 className="text-3xl font-bold mt-4 mb-4 ml-5"><i style={{color:"purple"}}>Shop Now!</i></h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 " style={{margin:"1rem"}}>
+      />
+      <h1 className="text-3xl font-bold mt-4 mb-4 ml-5"><i style={{ color: "purple" }}>Shop Now!</i></h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 " style={{ margin: "1rem" }}>
         {filteredProducts.map((product, index) => (
           <div key={index} className="border p-4 rounded shadow transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" style={{ height: '450px' }}>
             <img className="w-full h-48 object-cover mb-4" src={product.thumbnail} alt="" />
             <h1 className="font-bold text-lg mt-2">{product.title}</h1>
             <h2 className="text-base font-semibold mb-2">{product.name}</h2>
-            <p className="text-black mb-2" style={{ color: "purple", fontWeight:"bold" }}>Price: ${product.price}</p>
+            <p className="text-black mb-2" style={{ color: "purple", fontWeight: "bold" }}>Price: ${product.price}</p>
             <Link to={`/product/${product.id}`} className="text-gray-900 bg-gradient-to-r from-cyan-600 via-blue-300 to-purple-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-purple-100 dark:focus:ring-red-10 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >
               View Product
             </Link>
