e27c7da4b8106605165b274b902d5e27aad09354
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-02-18 17:54:01 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
             <div className="ml-4 flex items-center md:ml-6">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link to="/home" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-                <Link to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
                 <Link to="/Cart" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Cart</Link>
+                <Link to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
                 <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Logout</Link>
               </div>
             </div>
